cbc85266e148d80b8ce49197ac53289755718a3a
israel.lopez <israel.lopez@iesnervion.es>
2025-10-16 13:48:20 +0200

    Correccion de formato exercise 09

--- src/Diagramas/Exercise09.java
+++ src/Diagramas/Exercise09.java
@@ -15,9 +15,10 @@ public class Exercise09 {
 		System.out.println("Introduzca su calificación para comprobar su nota:");
 		num = sc.nextInt();
 
-		//Comprobamos cual cual es la nota alfabetica correspondiente y se las mostramos
+		// Comprobamos cual cual es la nota alfabetica correspondiente y se las
+		// mostramos
 		if (num >= 0) {
-			if (num >= 3) { 
+			if (num >= 3) {
 				if (num >= 5) {
 					if (num >= 7) {
 						if (num >= 9) {
@@ -25,24 +26,24 @@ public class Exercise09 {
 								System.out.println("La nota no puede ser mayor que 10.");
 							} else {
 								System.out.println("Sobresaliente");
-							}//Fin else nota entre 9 y 10
+							} // Fin else nota entre 9 y 10
 						} else {
 							System.out.println("Notable");
-						}//Fin else nota entre 7 y 8.99
+						} // Fin else nota entre 7 y 8.99
 					} else {
 						System.out.println("Bien");
-					}//Fin else nota entre 5 y 6.99
+					} // Fin else nota entre 5 y 6.99
 				} else {
 					System.out.println("Insuficiente");
-				}//Fin else nota entre 3 y 4.99
+				} // Fin else nota entre 3 y 4.99
 			} else {
 				System.out.println("Muy Deficiente");
-			}//Fin else nota entre 0 y 2.99
-			
+			} // Fin else nota entre 0 y 2.99
+
 		} else {
 			System.out.println("La nota no puede ser menor de 0.");
-		}//Fin else nota menor que 0
-		
+		} // Fin else nota menor que 0
+
 		sc.close();
 	}
 }
