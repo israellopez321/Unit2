47641a0975075bd2d5bf988017d9ecac4d3906d6
israel.lopez <israel.lopez@iesnervion.es>
2025-10-16 13:47:20 +0200

    Comentario a los elses

--- src/Diagramas/Exercise09.java
+++ src/Diagramas/Exercise09.java
@@ -25,23 +25,23 @@ public class Exercise09 {
 								System.out.println("La nota no puede ser mayor que 10.");
 							} else {
 								System.out.println("Sobresaliente");
-							}
+							}//Fin else nota entre 9 y 10
 						} else {
 							System.out.println("Notable");
-						}
+						}//Fin else nota entre 7 y 8.99
 					} else {
 						System.out.println("Bien");
-					}
+					}//Fin else nota entre 5 y 6.99
 				} else {
 					System.out.println("Insuficiente");
-				}
+				}//Fin else nota entre 3 y 4.99
 			} else {
 				System.out.println("Muy Deficiente");
-			}
+			}//Fin else nota entre 0 y 2.99
 			
 		} else {
 			System.out.println("La nota no puede ser menor de 0.");
-		}
+		}//Fin else nota menor que 0
 		
 		sc.close();
 	}
