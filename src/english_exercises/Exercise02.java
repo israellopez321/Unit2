fb37223a2cf8dddf4962e493626f363c46fc3da9
israellopez321 <israel.lopez@iesnervion.es>
2025-10-27 08:58:03 +0100

    a

--- src/english_exercises/Exercise02.java
+++ src/english_exercises/Exercise02.java
@@ -3,8 +3,15 @@ package english_exercises;
 public class Exercise02 {
 
 	public static void main(String[] args) {
+		//Creation a new Scanner
+		Scanner sc = new Scanner(System.in);
 		
-
+		//Declaration a variable for contain amount of money 
+		double money;
+		
+		//Declaration a variable for contain constant of interest
+		final double interestA = 1.5;
+		
 	}
 
 }
