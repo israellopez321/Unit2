0dfd700ebe3639f86575e02baf02988a0b3bd218
darkx <israel.lopez@iesnervion.com>
2025-11-04 20:08:02 +0100

    Exercise while

--- src/while_/Exercise04.java
+++ src/while_/Exercise04.java
@@ -4,27 +4,27 @@ import java.util.Scanner;
 
 public class Exercise04 {
 	public static void main(String[] args) {
-		//Creamos el escaner
+		// Creamos el escaner
 		Scanner sc = new Scanner(System.in);
 		
-		//Declaramos una variable para contener los numeros introducidos por el usuario
+		// Declaramos una variable para contener los numeros introducidos por el usuario
 		int numero;
 		
-		//Declaramos unas variables para contener los contadores
+		// Declaramos unas variables para contener los contadores
 		int contador = 0;
 		int contadorCero = 0;
 		int contadorNeg = 0;
 		
-		//Declaramos una varibles para contener la suma y media de los numeros introducidos
+		// Declaramos una varibles para contener la suma y media de los numeros introducidos
 		int sumaPos = 0;
 		int sumaNeg = 0;
 		double mediaNeg;
 		
-		//Le pedimos al usuario que introduzca un numero entero
+		// Le pedimos al usuario que introduzca un numero entero
 		System.out.println("Introduzca un número entero:");
 		numero = sc.nextInt();
 		
-		//Usamos while para clasificar los numeros positivos, negativos y ceros
+		// Usamos while para clasificar los numeros positivos, negativos y ceros
 		while (contador < 10) {
 			if (numero > 0) {
 				sumaPos += numero;
@@ -36,18 +36,18 @@ public class Exercise04 {
 				sumaNeg += numero;
 				contadorNeg++;
 				
-			}//Cerramos else
+			}// Cerramos else
 			contador++;
 			
-			//Le pedimos al usuario que introduzca un numero entero
+			// Le pedimos al usuario que introduzca un numero entero
 			System.out.println("Introduzca un número entero:");
 			numero = sc.nextInt();
 		}
 		
-		//Calculamos la media de numeros negativos
+		// Calculamos la media de numeros negativos
 		mediaNeg = sumaNeg / contadorNeg;
 		
-		//Le mostasmos al usuario la suma de los numeros positivos, el contador de ceros y la media de numeros negativos
+		// Le mostasmos al usuario la suma de los numeros positivos, el contador de ceros y la media de numeros negativos
 		System.out.println("Suma de números positivo: " + sumaPos);
 		System.out.println("Media de números negativos: " + mediaNeg);
 		System.out.println("Contador de ceros: " + contadorCero);
--- src/while_/Exercise05.java
+++ src/while_/Exercise05.java
@@ -1,5 +1,48 @@
 package while_;
 
-public class Exercise05 {
+import java.util.Scanner;
 
+public class Exercise05 {
+	public static void main(String[] args) {
+		// Creamos un escaner
+		Scanner sc = new Scanner(System.in);
+		
+		// Declaramos una variable para contener las edades introducidas por el usuario
+		int edad;
+		
+		// Declaramos unas variables para contener un contador, la suma de las edades y el contador de mayores de edad
+		int contador = 0;
+		int suma = 0;
+		int contadorMayores = 0;
+		
+		//Declaramos una variable para contener la media de edad
+		double media;
+		
+		//Le pedimos al usuario que introduzca una edad
+		System.out.println("Introduzca una edad, en caso de que quiera terminar el programa añada una edad negativa: ");
+		edad = sc.nextInt();
+		
+		//Usamos while para calcular la suma de todas las edades introducidas, la media, el número de alumnos y cuántos son mayores de edad.
+		while (edad >= 0) {
+			contador++;
+			suma += edad;
+			if (edad >= 18) {
+				contadorMayores++;
+			}
+			System.out.println("Introduzca una edadm en caso de que quiera terminar el programa añada una edad negativa: ");
+			edad = sc.nextInt();
+		}
+		
+		//Calculamos la media de edad
+		media = suma / contador;
+		
+		//Le mostramos al usuario la suma de todas las edades introducidas, la media, el número de alumnos y cuántos son mayores de edad.
+		System.out.println("Suma de edades: " + suma);
+		System.out.println("Media de edades: " + media);
+		System.out.println("Número de alumnos: " + contador);
+		System.out.println("Mayores de edad: " + contadorMayores);
+		
+		//Cerramos escaner
+		sc.close();
+	}
 }
