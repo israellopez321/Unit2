0edcddb836dd59cff458dd6514816715c92e293d
israellopez321 <israel.lopez@iesnervion.es>
2025-11-14 13:36:03 +0100

    Exercise02 bucles

---
+++ src/bucles/Exercise02.java
@@ -0,0 +1,16 @@
+package bucles;
+
+public class Exercise02 {
+	public static void main(String[] args) {
+		//Creamos un escaner
+		Scanner sc = new Scanner(System.in);
+		
+		//Numero introducido por el usuario 
+		int num;
+		
+		for (int cont = 1; cont <= num ; cont++ ) {
+			
+			
+		}
+	}
+}
