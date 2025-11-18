102287fb80c4081841c19cf8a6bc8dd8f663a97c
israellopez321 <israel.lopez@iesnervion.es>
2025-11-18 14:10:11 +0100

    update exercise 02 and 03

--- src/bucles/Exercise02.java
+++ src/bucles/Exercise02.java
@@ -1,16 +1,35 @@
 package bucles;
 
+import java.util.Scanner;
+
 public class Exercise02 {
 	public static void main(String[] args) {
 		//Creamos un escaner
 		Scanner sc = new Scanner(System.in);
 		
-		//Numero introducido por el usuario 
+		// Numero introducido por el usuario 
 		int num;
+		
+		// Variable para saber si es primo o no
+		boolean esPrimo;
+		
+		// Contador de numeros primos
+		int contadorPrimos = 0;
+		
+		//Le preguntamos al usuario por el número
+		System.out.println("Introduzca un número:");
+		num = sc.nextInt();
+		
 		
 		for (int cont = 1; cont <= num ; cont++ ) {
 			
+			if (cont%2 == 0 || cont%3 == 0 || cont%5== 0) {
+				contadorPrimos += 1;
+				}
 			
 		}
+		
+		// Le mostramos al usuario el resultado
+		System.out.println("Entre el 1 y el " + num + " hay " + contadorPrimos + " números primos.");
 	}
 }
---
+++ src/bucles/Exercise03.java
@@ -0,0 +1,14 @@
+package bucles;
+
+public class Exercise03 {
+	public static void main(String[] args) {
+		//Creamos un escaner
+		Scanner sc = new Scanner(System.in);
+		
+		//Declaramos una variable para contener dos numeros
+		int num1;
+		int num2;
+		
+		//Le pedimos al usuario que
+	}
+}
