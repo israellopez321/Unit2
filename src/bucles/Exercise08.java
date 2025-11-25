fd680289903441202339e7312c9313bcbfe5108c
israellopez321 <israel.lopez@iesnervion.es>
2025-11-25 13:48:49 +0100

    Exercise 08 // Bucles // Unit2

---
+++ src/bucles/Exercise08.java
@@ -0,0 +1,38 @@
+package bucles;
+
+import java.util.Scanner;
+
+public class Exercise08 {
+	public static void main(String[] args) {
+		//Creamos un escaner
+		Scanner sc = new Scanner(System.in);
+		
+		//Variable para guardar los dos números introducidos por el usuario
+		int num1;
+		int num2;
+		
+		//Variable para contador número introducidos
+		int numIntroducidos;
+		
+		//Variable para contador números fallados
+		int numFallo;
+		
+		//Le preguntamos al usuario por un numero inicial
+		System.out.print("Introduzca el número inicial: ");
+		
+		//Bucle para preguntar por los números
+		do {
+			//Pedimos al usuario un números
+			System.out.print("Dime un número: ");
+			num1 = sc.nextInt();
+			//Pedimos al usuario otro número
+			System.out.print("Dime un número: ");
+			num2 = sc.nextInt();
+			if (num1 < )
+			
+			
+			numIntroducido = num2;
+		} while(num1 != 0 || num2 != 0);
+		
+	}
+}
