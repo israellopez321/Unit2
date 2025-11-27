a9ecc71cdcdde7076694245c92ac2490bf9e1dc5
israellopez321 <israel.lopez@iesnervion.es>
2025-11-27 13:04:18 +0100

    Exercise 08 // Bucles // Unit 2

--- src/bucles/Exercise08.java
+++ src/bucles/Exercise08.java
@@ -12,27 +12,40 @@ public class Exercise08 {
 		int num2;
 		
 		//Variable para contador número introducidos
-		int numIntroducidos;
+		int numIntroducidos = 0;
 		
 		//Variable para contador números fallados
-		int numFallo;
+		int numFallo = 0;
+		
+		//Variable para contener el ultimo número introducido
+		int numUltimo;
 		
 		//Le preguntamos al usuario por un numero inicial
 		System.out.print("Introduzca el número inicial: ");
+		numUltimo = sc.nextInt();
 		
 		//Bucle para preguntar por los números
 		do {
 			//Pedimos al usuario un números
 			System.out.print("Dime un número: ");
 			num1 = sc.nextInt();
+			numIntroducidos++;
+			
 			//Pedimos al usuario otro número
 			System.out.print("Dime un número: ");
 			num2 = sc.nextInt();
-			if (num1 < )
+			numIntroducidos++;
 			
-			
-			numIntroducido = num2;
+			if (num2 < numUltimo ) {
+				numFallo++;
+				System.out.println("Fallo es menor");
+			}
+			numUltimo = num2;
 		} while(num1 != 0 || num2 != 0);
 		
+		System.out.println("Total de números introducidos: " + numIntroducidos);
+		System.out.println("Número fallados: " + numFallo);
+		
+		sc.close();
 	}
 }
